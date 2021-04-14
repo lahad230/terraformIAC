0637e3cee4ffab92d17d9e9d227ead098f8fdffa
lahad230 <lahad230@gmail.com>
2021-04-14 10:56:12 +0300

    changes to variables.tf

--- variables.tf
+++ variables.tf
@@ -1,36 +1,37 @@
 variable "vmSize" {
-  type        = string
   description = "size of both web vms and db vms."
+  type        = string
+  default     = "Standard_B2s"
 }
 
 variable "location" {
+  description = "location of all the resources. eg-> Germany West Central"
   type        = string
-  description = "location of all the resources."
 }
 
 variable "username" {
+  description = "all vms username. eg-> myUsername"
   type        = string
-  description = "all vms username."
 }
 
 variable "password" {
+  description = "all vms passwords. eg->sTRongPasswoRd"
   type        = string
-  description = "all vms passwords."
 }
 
 variable "resourceGroupName" {
-  type        = string
   description = "main resource group name."
+  type        = string
 }
 
 variable "vNet" {
+  description = "name and cidr for Vnet. eg-> name = 'Vnet' cidr = '10.0.0.0/16'" 
   type        = map
-  description = "name and cidr for Vnet."
 }
 
 variable "publicSubnet" {
+  description = "public subnet cidr, name and associated nsg name. eg-> name ='mysubenet' cidr = '10.0.1.0/28' nsgName = 'ndgForsubnet'"
   type        = map
-  description = "public subnet cidr, name and associated nsg name."
 }
 
 variable "privateSubnet" {
@@ -38,22 +39,23 @@ variable "privateSubnet" {
   description = "private subnet cidr, name and associated nsg name."
 }
 
+variable "privateNat" {
+  type        = string
+  description = "name for private subnet nat gateway."
+
+}
+
 variable "natPublicIpName" {
   type        = string
   description = "name for the public ip for the private subnet nat gateway."
 }
 
-variable "privateNat" {
-  type        = string
-  description = "name for private subnet nat gateway."
-}
-
 variable "publicLb" {
   type        = map
-  description = "public load balancer details (name, front ip name and public ip name)."
+  description = "public load balancer details (name, front ip name and public ip name). eg-> name = 'publicLb' ipName = 'lbIp' frontIpName = 'lbForntIp'"
 }
 
 variable "privateLb" {
   type        = map
-  description = "private load balancer details (name, front ip name, private ip[remember: private ip depandes on the private subnet cidr])."
+  description = "private load balancer details (name, front ip name, private ip[remember: private ip depandes on the private subnet cidr] eg-> name = 'privateLb' frontIpName = 'privateFront' privateIp = '10.0.1.10')."
 }
